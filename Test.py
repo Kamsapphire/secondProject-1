bcd5fc9dedb7d89901e57f08fe0c3cf3b5cfe00e
kamranzaidi-devops <39591844+kamranzaidi-devops@users.noreply.github.com>
2022-07-02 00:16:21 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,5 +1,21 @@
-from main import print_hi
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-class Test:
-    print_hi
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+        num1 = 1.5
+        num2 = 6.3
+
+        # Add two numbers
+        sum = num1 + num2
+
+        # Display the sum
+        print("this is testing framework")
